43557023d84b53428bdf67ba9b3763ba2ee3e9a4
Gabriel-Troni <troni.gabriel.10@gmail.com>
2024-06-10 22:16:06 -0300

    Iniciadas Conta1 e Conta

--- Source Packages/br/com/banking_system/Conta.java
+++ Source Packages/br/com/banking_system/Conta.java
@@ -1,3 +1,79 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
 package br.com.banking_system;
 
-/* Classe *abstrata* conta */
+
+
+/*
+
+Não ta   pronto ainda!!!
+
+
+
+
+*/
+
+
+
+/**
+ *
+ * @author gabrieltroni
+ */
+public abstract class Conta 
+    implements Conta1 {
+    private Cliente cliente;
+    private double saldo;
+    private int numConta;
+    
+    public void Conta(Cliente cliente){
+        this.cliente = cliente;
+        this.numConta = geraNumero();
+        this.saldo = 0;
+    }
+    
+    @Override
+    public boolean deposita(double valor){
+        if(valor>0){
+            saldo += valor;
+            return true;
+        }
+        return false;
+    }
+    
+    @Override
+    public boolean saca(double valor){
+        if(valor > 0 && valor <= saldo){
+            saldo -= valor;
+            return true;
+        }
+        return false;
+    }
+    
+    @Override
+    public String getDono(){
+        return Cliente.getNome();
+    }
+    
+    @Override
+    public int getNumero(){
+        return numConta;
+    }
+    
+    @Override
+    public double getSaldo(){
+        return saldo;
+    }
+    
+    @Override
+    public abstract boolean remunera();
+    
+    public int geraNumero(){
+        return 0; // descobrir como faz isso
+    }
+    
+    public void vinculaCliente(Cliente cliente){
+        this.cliente = cliente;
+    }
+}
