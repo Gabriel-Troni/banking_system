5ad39f9912683f262d9f4ee65685debc7d42f175
tiago_vita <tiago.vita@ufpr.br>
2024-06-12 10:01:21 -0300

    Composição da ContaCorrente

--- Source Packages/br/com/banking_system/ContaCorrente.java
+++ Source Packages/br/com/banking_system/ContaCorrente.java
@@ -1,3 +1,38 @@
 package br.com.banking_system;
 
-/* classe ContaCorrente (herdar de Conta) */
+public class ContaCorrente extends Conta {
+
+    private Double limite;
+
+    public ContaCorrente(Double limite) {
+        this.limite = limite;
+    }
+
+    @Override
+    public boolean saca(double valor) {
+        if(valor > 0 && valor <= saldo + limite){
+            saldo -= valor;
+            return true;
+        }
+        return false;
+    }
+    
+    @Override
+    public void remunera() {
+    	if(saldo >= 0) {
+    		saldo = saldo*1.01;
+    	}
+    }
+    
+    public boolean atualizaLimite(double valor) {
+    	if(valor >= 0) {
+    		limite = valor;
+    		return true;
+    	}
+    	return false;
+    }
+    
+    public double getLimite() {
+    	return limite;
+    }
+}
