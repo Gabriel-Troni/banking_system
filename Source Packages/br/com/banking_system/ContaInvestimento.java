ae217da2c1670b601b63950e3f94fd426c141525
Ana Beatriz Rosa Henrique <anabeatriz0677@gmail.com>
2024-06-13 20:06:51 -0300

    Criação da classe ContaInvestimento

--- Source Packages/br/com/banking_system/ContaInvestimento.java
+++ Source Packages/br/com/banking_system/ContaInvestimento.java
@@ -1,3 +1,48 @@
 package br.com.banking_system;
 
 /* classe ContaInvestimento (herdar de Conta) */
+
+/**
+ *
+ * @author anaBeatriz
+ */
+public class ContaInvestimento extends Conta implements Conta1 {
+    
+    private double depositoMinimo;
+    private double montanteMinimo;
+    
+    public ContaInvestimento(double saldo, int numero, String dono, double depositoMinimo, double montanteMinimo) {
+        super(saldo, numero, dono);
+        this.depositoMinimo = depositoMinimo;
+        this.montanteMinimo = montanteMinimo;
+    }
+    
+    @Override
+    public boolean saca(double valor) {
+        if(valor>0 && (this.saldo-valor >= montanteMinimo )){
+            super.saca(valor);
+            return true;
+        }else{
+            System.out.println("Saque não realizado. Verifique se o valor é válido ou se o saldo mínimo é atendido.");  
+            return false;   
+        }      
+    }
+    
+    
+    @Override
+    public boolean deposita(double valor) {
+        if(valor>0 && valor>=this.depositoMinimo){
+            super.deposita(valor);
+            return true;
+        }else{
+            System.out.println("Depósito não realizado. Verifique se o valor é válido ou atende ao depósito mínimo.");
+            return false;
+        }
+    }
+    
+    @Override
+    public void remunera() {
+       this.saldo += this.saldo*0.02;
+    }
+}   
+    
