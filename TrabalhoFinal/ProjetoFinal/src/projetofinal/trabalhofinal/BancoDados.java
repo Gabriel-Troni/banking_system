1e0c8e1ca060cd32760c0efe61b88bf64207ab98
rodolfo9254 <rodolfo9254@gmail.com>
2024-06-13 21:59:17 -0300

    criado novo projeto

---
+++ TrabalhoFinal/ProjetoFinal/src/projetofinal/trabalhofinal/BancoDados.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package trabalhofinal;
+
+import java.util.List;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Rodolfo
+ */
+public class BancoDados {
+    public static List<Cliente> listaClientes = new ArrayList<Cliente>();
+    public static List<Conta> listaContas = new ArrayList<Conta>(); 
+    
+}
